adbd121b3a643f6aa16ef20214333ceeca019d15
olha-hoshko <olha.hoshko.w@gmail.com>
2026-02-22 20:51:24 +0200

    removed Optional

--- components/comment_item_component.py
+++ components/comment_item_component.py
@@ -1,9 +1,8 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
-from typing import Optional
 from base_component import BaseComponent
 
 class CommentItemComponent(BaseComponent):
 
-    def __init__(self, driver: WebDriver, root: Optional[WebElement], timeout):
+    def __init__(self, driver: WebDriver, root: WebElement, timeout):
         super().__init__(root, driver, timeout)
