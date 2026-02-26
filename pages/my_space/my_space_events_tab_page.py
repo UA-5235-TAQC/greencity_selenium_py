7166285ecc2be49335e74a843442a6f42115590b
Oleksandr Kolodiichuk <sashkoko05@gmail.com>
2026-02-26 10:43:01 +0100

    Rewrite MySpaceEventsTabPage using PageFactory

--- pages/my_space/my_space_events_tab_page.py
+++ pages/my_space/my_space_events_tab_page.py
@@ -1,55 +1,67 @@
+from typing import List
+import allure
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 from pages.my_space.my_space_base_page import MySpaceBasePage
+from utils.page_factory import LocatorsTable
 
 class MySpaceEventsTabPage(MySpaceBasePage):
+    events_header: WebElement
+    filters: WebElement
+    events_items: List[WebElement]
+    online_checkbox: WebElement
+    offline_checkbox: WebElement
+    add_event_button: WebElement
+    join_event_button: WebElement
+    no_data_component: WebElement
 
-    _EVENTS_HEADER = (By.CSS_SELECTOR, ".header")
-    _FILTERS = (By.CSS_SELECTOR, ".events-filter")
-    _EVENTS_LIST = (By.XPATH, "//div[contains(@class,'scrolling')]//app-event-item")
-    _ONLINE_CHECKBOX = (By.XPATH, "//mat-checkbox[.//label[normalize-space()='Online']]//input")
-    _OFFLINE_CHECKBOX = (By.XPATH, "//mat-checkbox[.//label[normalize-space()='Offline']]//input")
-    _ADD_EVENT_BUTTON = (By.ID, "create-button-event")
-    _JOIN_EVENT_BUTTON = (By.ID, "create-button-join-event")
-    _NO_DATA_COMPONENT = (By.CSS_SELECTOR, ".no-data app-no-data")
+    locators: LocatorsTable = {
+        "events_header": (By.CSS_SELECTOR, ".header"),
+        "filters": (By.CSS_SELECTOR, ".events-filter"),
+        "events_items": (By.CSS_SELECTOR, "app-event-item"),
+        "online_checkbox": (By.XPATH, "//mat-checkbox[.//label[normalize-space()='Online']]//input"),
+        "offline_checkbox": (By.XPATH, "//mat-checkbox[.//label[normalize-space()='Offline']]//input"),
+        "add_event_button": (By.ID, "create-button-event"),
+        "join_event_button": (By.ID, "create-button-join-event"),
+        "no_data_component": (By.CSS_SELECTOR, ".no-data app-no-data"),
+    }
 
-    def get_events_header(self):
-        return self.wait.until(EC.visibility_of_element_located(self._EVENTS_HEADER))
+    @allure.step("Get events")
+    def get_events(self) -> List[WebElement]:
+        return self.events_items
 
-    def get_filters(self):
-        return self.wait.until(EC.visibility_of_element_located(self._FILTERS))
-
-    def get_events(self):
-        return self.driver.find_elements(*self._EVENTS_LIST)
-
+    @allure.step("Get events count")
     def get_events_count(self) -> int:
-        return len(self.get_events())
+        return len(self.events_items)
 
+    @allure.step("Filter online events")
     def filter_online(self):
-        self.wait.until(EC.element_to_be_clickable(self._ONLINE_CHECKBOX)).click()
+        self.online_checkbox.click()
         return self
 
+    @allure.step("Filter offline events")
     def filter_offline(self):
-        self.wait.until(EC.element_to_be_clickable(self._OFFLINE_CHECKBOX)).click()
+        self.offline_checkbox.click()
         return self
 
+    @allure.step("Enable online filter")
     def enable_online(self):
-        checkbox = self.wait.until(EC.presence_of_element_located(self._ONLINE_CHECKBOX))
-        if not checkbox.is_selected():
-            checkbox.click()
+        if not self.online_checkbox.is_selected():
+            self.online_checkbox.click()
         return self
 
+    @allure.step("Enable offline filter")
     def enable_offline(self):
-        checkbox = self.wait.until(EC.presence_of_element_located(self._OFFLINE_CHECKBOX))
-        if not checkbox.is_selected():
-            checkbox.click()
+        if not self.offline_checkbox.is_selected():
+            self.offline_checkbox.click()
         return self
 
+    @allure.step("Click Add Event button")
     def click_add_event(self):
-        self.wait.until(EC.element_to_be_clickable(self._ADD_EVENT_BUTTON)).click()
+        self.add_event_button.click()
         return self
 
+    @allure.step("Click Join Event button")
     def click_join_event(self):
-        self.wait.until(EC.element_to_be_clickable(self._JOIN_EVENT_BUTTON)).click()
+        self.join_event_button.click()
         return self
