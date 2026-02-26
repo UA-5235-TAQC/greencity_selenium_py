d17825da7a738cceffcd36c1460fe9472a4f8daa
Oleksandr Kolodiichuk <sashkoko05@gmail.com>
2026-02-26 10:29:50 +0100

    Rewrite MySpaceHabitsTabPage using PageFactory

--- pages/my_space/my_space_habits_tab_page.py
+++ pages/my_space/my_space_habits_tab_page.py
@@ -1,16 +1,25 @@
+from typing import List
+import allure
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from pages.my_space.my_space_base_page import MySpaceBasePage
+from utils.page_factory import LocatorsTable
 
 class MySpaceHabitsTabPage(MySpaceBasePage):
 
-    _NO_DATA_COMPONENT = (By.CSS_SELECTOR, ".no-data app-no-data")
-    _ADD_HABIT_BUTTON = (By.ID, "create-button-add-new-habit")
+    no_data_components: List[WebElement] 
+    add_habit_button: WebElement
 
+    locators: LocatorsTable = {
+        "no_data_components": (By.CSS_SELECTOR, ".no-data app-no-data"),
+        "add_habit_button": (By.ID, "create-button-add-new-habit"),
+    }
+
+    @allure.step("Check if user has habits")
     def has_habits(self) -> bool:
-        elements = self.driver.find_elements(*self._NO_DATA_COMPONENT)
-        return len(elements) == 0
+        return len(self.no_data_components) == 0
 
+    @allure.step("Click Add Habit button")
     def click_add_habit(self):
-        self.wait.until(EC.element_to_be_clickable(self._ADD_HABIT_BUTTON)).click()
+        self.add_habit_button.click()
         return self
